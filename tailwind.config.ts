79e63421a828b5b217888429799d5978c413989e
Brian Ho <YWHo@users.noreply.github.com>
2024-02-05 15:07:23 +1300

    setup customised color and font in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -8,10 +9,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#038C7F",
+        secondary: "#038C7F",
+        tertiary: {
+          dark: "#F27405",
+          light: "#F2c641",
+        },
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)", ...fontFamily.sans],
       },
     },
   },
