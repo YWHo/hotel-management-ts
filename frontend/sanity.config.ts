5b0fa10f4b6461fdb19f5a940e946f289d56ff1e
Brian Ho <YWHo@users.noreply.github.com>
2024-02-15 18:37:37 +1300

    enable sanity studio at the frontend app/studio route

---
+++ frontend/sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
+
+export default defineConfig({
+  name: "default",
+  title: "hotel-management-ts",
+
+  projectId: "rf3r4puy",
+  dataset: "production",
+
+  basePath: "/studio",
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
